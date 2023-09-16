6d510761000d8a9c09f8b8e20bf6035d88204aeb
Fareed Khan <127206523+Fareedkhan56@users.noreply.github.com>
2023-09-16 17:12:41 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string value;
+
+            do
+            {
+                Console.Write("Enter First Value : ");
+                int firstValue = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Enter symbol '+, -, 8, /, %' : ");
+                string symbol = Console.ReadLine();
+
+                Console.Write("Enter Secnod Value : ");
+                int secondValue = Convert.ToInt32(Console.ReadLine());
+
+                int result;
+
+                switch (symbol)
+                {
+                    case "+":
+                        result = firstValue + secondValue;
+                        Console.WriteLine("here is result {0}", result); break;
+
+                    case "-":
+                        result = firstValue - secondValue;
+                        Console.WriteLine("here is result {0}", result); break;
+
+                    case "*":
+                        result = firstValue * secondValue;
+                        Console.WriteLine("here is result {0}", result); break;
+
+                    case "/":
+                        result = firstValue / secondValue;
+                        Console.WriteLine("here is result {0}", result); break;
+
+                    case "%":
+                        result = firstValue % secondValue;
+                        Console.WriteLine("here is result {0}", result); break;
+
+                    default:
+                        Console.Write("something wrong in input"); break;
+                }
+                Console.WriteLine("Do you want to continue (y,n)");
+                value = Console.ReadLine();
+            } while (value == "y" || value == "Y");
+            
+
+            Console.ReadLine();
+        }
+    }
+}
